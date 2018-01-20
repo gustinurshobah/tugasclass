1affff8e9993f6bc0d37121d9af5ab8ebc03d357
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2018-01-20 15:41:09 +0800

    menginfut  type class sepedamotor

--- tugasclass.cpp
+++ tugasclass.cpp
@@ -6,4 +6,15 @@ class SEPEDAMOTOR{
 	public:
 		void suara(string bunyi);
 		string warna, type;
+		
 };
+
+int main(){
+	SEPEDAMOTOR MATIC, SPORTY, CUB;
+	
+	MATIC.warna ="brown";
+	MATIC.type  ="Scoopy";
+	SPORTY.warna ="orange";
+	SPORTY.type  ="CBR 150R";
+	CUB.warna = "hitam merah";
+	CUB.type ="supra";
